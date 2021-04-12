556a67db7751a2883814dd3249a4ad8df04bec1c
Yuguobin-developer <xixian95@outlook.com>
2021-04-12 23:36:34 +0800

    update

--- app/Model/PodcastUser.php
+++ app/Model/PodcastUser.php
@@ -9,9 +9,4 @@ class PodcastUser extends Model
     //
     protected $table = "podcastusers";
     protected $fillable = ['userid','podcast_id'];
-
-    function createrinfo()
-    {
-    	return $this->belongsTo(User::class,'creator');
-    }
 }
